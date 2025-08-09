1b30b6aceeeed09c66f4afa11fefac8cd71b030f
Your Name <your.email@example.com>
2025-08-09 18:09:38 +0300

    batman

---
+++ archive/snippets.js
@@ -0,0 +1,22 @@
+/* let username
+const usernameSubmitButton = document.getElementById('usernameSubmitButton')
+usernameSubmitButton.onclick = () => {
+  username = document.getElementById('usernameInput').value
+  document.getElementById('heading1').textContent = `Welcome, ${username}!`
+} */
+
+/* let userAge
+
+while (true){
+  let age = window.prompt('How old are you?')
+  age = Number(age)
+  if (isNaN(age)) {
+    alert('Please enter a number')
+    continue
+  }
+
+  userAge = Number(age)
+  break
+}
+
+console.log(userAge) */
